f72990057dbf07279022c226bc53159928ff327b
FHieser <47272854+FHieser@users.noreply.github.com>
2026-08-23 01:07:15 +0200

    Add teachings of the mist

--- path-webapp/js/content-index.js
+++ path-webapp/js/content-index.js
@@ -140,7 +140,8 @@ const CONTENT_INDEX = {
     "dont-wake-the-beast.yaml",
     "guardian-projection.yaml",
     "in-the-eye-of-the-titan.yaml",
-    "left-to-the-mist.yaml"
+    "left-to-the-mist.yaml",
+    "teachings-of-the-mist.yaml"
   ],
   "__factions__": {
     "brass-mechanicorum": {
